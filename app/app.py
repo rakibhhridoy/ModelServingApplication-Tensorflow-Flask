0238dc3c99bef867705cae6b039a3b9e01436fa5
rakibhhridoy <rakibhridoy63@gmail.com>
2020-07-21 15:47:00 +0600

    appv1

---
+++ app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, url_for, request,redirect
+from flask_bootstrap import Bootstrap
+import os
+import inference
+
+app = Flask(__name__)
+
+Bootstrap(app)
+'''
+Routes
+'''
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            image_path = os.path.json('static', uploaded_file.filename)
+            uploaded_file.save(image_path)
+            class_name = inference.get_prediction(inage_path)
+            print('CLASS NAME=', class_name)
+            result = {
+                'class_name':class_name,
+                'image_path': image_path,
+            }
+
+            return render_template('show.html', result=result)
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
